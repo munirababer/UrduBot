fde9dc551f7bd81761ef4227a42bb27faf78e640
Munira Baber <88946068+munirababer@users.noreply.github.com>
2024-09-09 10:38:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def main():
     
     st.markdown("<h1 class='header-text'> Urdu Voice Chatbot</h1>", unsafe_allow_html=True)
     st.subheader('"اردو وائس باٹ" ', divider='blue')
-    st.sidebar.header("اردو چیت باٹ", divider='blue')
+    st.sidebar.header("اردو چیٹ باٹ", divider='blue')
     st.sidebar.write('''یہ ایک اردو وائس چیٹ بوٹ ہے۔ اس میں اردو وائس ان پٹ لیتا ہے اور اردو ٹیکسٹ کے ساتھ اردو آواز میں جواب دیتا ہے۔ ''')
 
    
     with st.container():
         st.markdown('<div class="audio-recorder-container">', unsafe_allow_html=True)
     # Audio recorder for Urdu input
-    audio_data = audio_recorder(text='آاوار ریکارڈ کرواعیں', icon_size="2x", icon_name="microphone-lines", key="urdu_recorder")
+    audio_data = audio_recorder(text='آاوار ریکارڈ کروائیں', icon_size="2x", icon_name="microphone-lines", key="urdu_recorder")
     st.markdown('</div>', unsafe_allow_html=True)
 
     if audio_data is not None:
@@ -68,7 +68,7 @@ def convert_audio_to_text(audio_file_path):
             text = recognizer.recognize_google(audio_data, language="ur")
             return text
         except sr.UnknownValueError:
-            return "آپ کی آواز نہیں ہے"
+            return "آپ کی آواز واضح نہیں ہے"
         except sr.RequestError:
             return "Sorry, my speech service is down"
 
